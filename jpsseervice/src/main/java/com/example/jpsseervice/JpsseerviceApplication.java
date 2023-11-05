5fbb752412baacee3201fc5ac10ba686d46ad7b2
jpohl <jpohl@web.de>
2023-11-05 18:07:15 +0100

    initial commit

---
+++ jpsseervice/src/main/java/com/example/jpsseervice/JpsseerviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpsseervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpsseerviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpsseerviceApplication.class, args);
+	}
+
+}
---
+++ jpsseervice/src/test/java/com/example/jpsseervice/JpsseerviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpsseervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpsseerviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
